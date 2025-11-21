7fb4b40234878c89b8963a7c4ff0d172aa4140fa
ansqhrl3037 <ansqhrl3037@gmail.com>
2025-11-21 16:27:27 +0900

    fix:d9 merge

--- pintos/userprog/process.c
+++ pintos/userprog/process.c
@@ -46,11 +46,6 @@ tid_t process_create_initd(const char *file_name)
 {
     char *fn_copy;
     tid_t tid;
-    char *tmp;
-    char *file_name_r = palloc_get_page(0);
-
-    strlcpy(file_name_r, file_name, PGSIZE);
-    file_name_r = strtok_r(file_name_r, " ", &tmp);
 
     /* Make a copy of FILE_NAME.
      * Otherwise there's a race between the caller and load(). */
@@ -60,10 +55,9 @@ tid_t process_create_initd(const char *file_name)
     strlcpy(fn_copy, file_name, PGSIZE);
 
     /* Create a new thread to execute FILE_NAME. */
-    tid = thread_create(file_name_r, PRI_DEFAULT, initd, fn_copy);
+    tid = thread_create(file_name, PRI_DEFAULT, initd, fn_copy);
     if (tid == TID_ERROR)
         palloc_free_page(fn_copy);
-    palloc_free_page(file_name_r);
     return tid;
 }
 
